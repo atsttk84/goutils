604f4ddfba902962ab5a62dee3ff2c06b220a7ce
atsttk84 <atsttk84@gmail.com>
2023-01-29 19:46:24 +0900

    abtest

---
+++ mutex/ab.go
@@ -0,0 +1,75 @@
+package mutex
+
+import (
+	"math/rand"
+	"sync"
+	"time"
+)
+
+/*
+ https://go.dev/tour/concurrency/9
+*/
+
+// SafeCounter is safe to use concurrently.
+type SafeCounter struct {
+	mu sync.Mutex
+	v  map[int]int
+}
+
+// Value returns the current value of the counter for the given key.
+func (c *SafeCounter) Draw(key int) (v int) {
+	c.mu.Lock()
+	// Lock so only one goroutine at a time can access the map c.v.
+	defer c.mu.Unlock()
+	c.v[key]++
+	v = c.v[key]
+	return
+}
+
+type AbTest struct {
+	mu sync.Mutex
+	c  map[int]int
+	l  map[int][]int
+	r  int
+	t  int
+}
+
+func (ab *AbTest) Draw(key int) (v int) {
+	ab.mu.Lock()
+	// Lock so only one goroutine at a time can access the map c.v.
+	defer ab.mu.Unlock()
+	if _, ok := ab.l[key]; !ok {
+		ab.l[key] = initList(ab.r, ab.t)
+	}
+	v = ab.l[key][ab.c[key]]
+	ab.c[key]++
+	if len(ab.l[key]) <= ab.c[key] {
+		ab.c[key] = 0
+		ab.l[key] = initList(ab.r, ab.t)
+	} else {
+	}
+	return
+}
+
+func (ab *AbTest) Next(key int) (v, c int, l []int) {
+	ab.mu.Lock()
+	// Lock so only one goroutine at a time can access the map c.v.
+	defer ab.mu.Unlock()
+	l = ab.l[key]
+	c = ab.c[key]
+	v = ab.l[key][ab.c[key]]
+	return
+}
+
+func initList(true_rate, total int) (list []int) {
+  true_cnt := total * true_rate / 100
+	false_list := make([]int, total-true_cnt)
+	true_list := make([]int, true_cnt)
+	for i := range true_list {
+		true_list[i] = 1
+	}
+	list = append(false_list, true_list...)
+	rand.Seed(time.Now().UnixNano())
+	rand.Shuffle(len(list), func(i, j int) { list[i], list[j] = list[j], list[i] })
+	return
+}
---
+++ mutex/ab_test.go
@@ -0,0 +1,60 @@
+package mutex
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestSafeCounterTest(t *testing.T) {
+	sc := SafeCounter{v: make(map[int]int)}
+	for i := 0; i < 11; i++ {
+		fmt.Println(sc.Draw(1))
+	}
+}
+
+func TestAbTest(t *testing.T) {
+	ab := AbTest{r: 30, t: 10, c: make(map[int]int), l: make(map[int][]int)}
+	for i := 0; i < 11; i++ {
+		fmt.Println(ab.Draw(1))
+		fmt.Println(ab.Next(1))
+	}
+}
+
+func TestInitList(t *testing.T) {
+	fmt.Println(initList(20, 10))
+}
+
+func BenchmarkSafeCounter(b *testing.B) {
+	sc := SafeCounter{v: make(map[int]int)}
+	for i := 0; i < b.N; i++ {
+		sc.Draw(1)
+	}
+}
+
+func BenchmarkAbTest100(b *testing.B) {
+	ab := AbTest{r: 5, t: 100, c: make(map[int]int), l: make(map[int][]int)}
+	for i := 0; i < b.N; i++ {
+		ab.Draw(1)
+	}
+}
+
+func BenchmarkAbTest10000(b *testing.B) {
+	ab := AbTest{r: 5, t: 10000, c: make(map[int]int), l: make(map[int][]int)}
+	for i := 0; i < b.N; i++ {
+		ab.Draw(1)
+	}
+}
+
+func BenchmarkAbTest100000(b *testing.B) {
+	ab := AbTest{r: 5, t: 100000, c: make(map[int]int), l: make(map[int][]int)}
+	for i := 0; i < b.N; i++ {
+		ab.Draw(1)
+	}
+}
+
+func BenchmarkAbTest1000000(b *testing.B) {
+	ab := AbTest{r: 5, t: 1000000, c: make(map[int]int), l: make(map[int][]int)}
+	for i := 0; i < b.N; i++ {
+		ab.Draw(1)
+	}
+}
